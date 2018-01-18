203c0f5dcfdcbe76a7fa490e3688307d82d41987
cynthiamao98 <32347565+cynthiamao98@users.noreply.github.com>
2018-01-17 22:55:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def main():
     return render_template('index.html')
 
 
+@app.route('/sm4396')
+def subpage():
+    return render_template('sm4396.html')
+
+
 if __name__ == '__main__':
     app.run()
